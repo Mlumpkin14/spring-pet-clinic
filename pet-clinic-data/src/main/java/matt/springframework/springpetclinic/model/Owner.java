e70e48b545e2b9444df983a14effe2d6c1264117
mlumpkin14 <mattlumpkin04@gmail.com>
2020-05-07 00:24:15 -0400

    Add Contact info Properties to Owners Closes #28

--- pet-clinic-data/src/main/java/matt/springframework/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/matt/springframework/springpetclinic/model/Owner.java
@@ -4,7 +4,35 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+
+    private String Address;
+    private String City;
+    private String Telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        Telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
