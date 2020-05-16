3798ff7d8f779cbcf60cab2e72f6b0d606ce84e6
mlumpkin14 <mattlumpkin04@gmail.com>
2020-05-16 02:37:05 -0400

    Add Project Lombok and Refactor Closes #51

--- pet-clinic-data/src/main/java/matt/springframework/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/matt/springframework/springpetclinic/model/Owner.java
@@ -1,56 +1,41 @@
 package matt.springframework.springpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
 
+    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone,
+                 Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
+
     @Column(name = "address")
-    private String Address;
+    private String address;
 
     @Column(name = "City")
-    private String City;
+    private String city;
 
     @Column(name = "telephone")
-    private String Telephone;
+    private String telephone;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    public String getAddress() {
-        return Address;
-    }
 
-    public void setAddress(String address) {
-        Address = address;
-    }
-
-    public String getCity() {
-        return City;
-    }
-
-    public void setCity(String city) {
-        City = city;
-    }
-
-    public String getTelephone() {
-        return Telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        Telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
 
 
